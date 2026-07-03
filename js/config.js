7d94829e3c3710f42a480367ce35a9d5a2a3186f
Hayashi-HP <56819196+Hayashi-HP@users.noreply.github.com>
2026-07-03 16:38:23 +0900

    Expose config globally

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 const RINCHAN_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbzy-Qjf_UDzA17FaUuyRIFf5oiAr-8OgSukDrjWcuNgo39xAH1k7Z4Q4zrqjPnVlRYC/exec"
 };
+window.RINCHAN_CONFIG = RINCHAN_CONFIG;
