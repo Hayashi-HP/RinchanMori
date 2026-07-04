f13fe176cc1ac9d20574082af557db2556d2d13a
Hayashi-HP <56819196+Hayashi-HP@users.noreply.github.com>
2026-07-04 20:54:32 +0900

    Load mypage compact css v1.0.44

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const RINCHAN_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbzy-Qjf_UDzA17FaUuyRIFf5oiAr-8OgSukDrjWcuNgo39xAH1k7Z4Q4zrqjPnVlRYC/exec",
-  UI_VERSION: "v1.0.43"
+  UI_VERSION: "v1.0.44"
 };
 window.RINCHAN_CONFIG = RINCHAN_CONFIG;
 
@@ -16,8 +16,9 @@ window.RINCHAN_CONFIG = RINCHAN_CONFIG;
       link.href = href;
       document.head.appendChild(link);
     }
-    addCss('rinchanUnifiedPageSystemCss', prefix + 'css/v1041-page-system.css?v=1043');
-    addCss('rinchanSoftInnerPanelsCss', prefix + 'css/v1042-soft-inner-panels.css?v=1043');
-    addCss('rinchanMypageTuneCss', prefix + 'css/v1043-mypage-tune.css?v=1043');
+    addCss('rinchanUnifiedPageSystemCss', prefix + 'css/v1041-page-system.css?v=1044');
+    addCss('rinchanSoftInnerPanelsCss', prefix + 'css/v1042-soft-inner-panels.css?v=1044');
+    addCss('rinchanMypageTuneCss', prefix + 'css/v1043-mypage-tune.css?v=1044');
+    addCss('rinchanMypageCompactCss', prefix + 'css/v1044-mypage-compact.css?v=1044');
   } catch (e) {}
 })();
