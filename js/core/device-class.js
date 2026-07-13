16615f74d00ee549049a4791671c3842c2746c37
Hayashi-HP <56819196+Hayashi-HP@users.noreply.github.com>
2026-07-13 14:14:48 +0900

    v1.5.0 load mobile foundation on every main page

--- js/core/device-class.js
+++ js/core/device-class.js
@@ -3,4 +3,14 @@
   const root = document.documentElement;
   if (/Android/i.test(ua)) root.classList.add('is-android');
   if (/iPhone|iPad|iPod/i.test(ua)) root.classList.add('is-ios');
+
+  if (!document.querySelector('link[data-rinchan-mobile-foundation]')) {
+    const script = document.currentScript;
+    const isPage = script && /\/pages\//.test(script.src || '');
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = (isPage ? '../' : '') + 'css/v150-mobile-foundation.css?v=150';
+    link.dataset.rinchanMobileFoundation = 'v1.5.0';
+    document.head.appendChild(link);
+  }
 })();
